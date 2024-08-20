517e1e97a484efdbb30e535a7f9c898523e59ad0
Hassan Golshani <hassan.golshani@lobox.com>
2024-08-20 14:41:58 +0330

    Implement main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.model.Person;
+import org.example.validator.PersonValidator;
+
 public class Main {
+
+    private static final PersonValidator PERSON_VALIDATOR = new PersonValidator();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person();
+        //set your data
+        PERSON_VALIDATOR.validate2(person);
+        PERSON_VALIDATOR.validate(person);
     }
 }
